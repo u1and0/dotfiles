b605819450b08c3e45e08d0e1e683011212c15b3
u1and0 <e01.ando60@gmail.com>
2017-12-20 20:59:13 +0900

    [mod]@.ipython/profile_default/ipython_config.py: IPAMincho comment out

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -627,6 +627,7 @@ c.AliasManager.user_aliases = [
 c.InteractiveShellApp.exec_lines = ['%load_ext autoreload', '%autoreload 2']
 
 # Adjust white space of matplotlib plot
+#c.InlineBackend.rc = {'font.family': 'IPAMincho',
 c.InlineBackend.rc = {'font.family': 'TakaoPGothic',
                       'figure.facecolor': (1, 1, 1, 1),
                       'savefig.dpi': 72,
