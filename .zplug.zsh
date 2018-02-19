bfbf44590c137e2fbadb0f42456d6c0e93228c3d
u1and0 <e01.ando60@gmail.com>
2018-02-19 19:08:06 +0900

    [rm]@.zshrc, .zplug.zsh: Remove auto-fu

--- .zplug.zsh
+++ .zplug.zsh
@@ -58,44 +58,6 @@ zplug "zsh-users/zsh-syntax-highlighting", defer:2
 # Auto completions 
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-completions"
-zplug "hchbaw/auto-fu.zsh", at:pu, frozen:1  # zsh automatic complete-word and list-choices
-
-function zle-line-init(){
-    auto-fu-init
-}
-zle -N zle-line-init
-zstyle ':completion:*' completer _oldlist _complete 
-zstyle ':completion:*:default' menu select  # Tabキーを押すと候補を選択できる
-
-
-# delete unambiguous prefix when accepting line
-function afu+delete-unambiguous-prefix () {
-    afu-clearing-maybe
-    local buf; buf="$BUFFER"
-    local bufc; bufc="$buffer_cur"
-    [[ -z "$cursor_new" ]] && cursor_new=-1
-    [[ "$buf[$cursor_new]" == ' ' ]] && return
-    [[ "$buf[$cursor_new]" == '/' ]] && return
-    ((afu_in_p == 1)) && [[ "$buf" != "$bufc" ]] && {
-        there are more than one completion candidates
-        zle afu+complete-word
-        [[ "$buf" == "$BUFFER" ]] && {
-            the completion suffix was an unambiguous prefix
-            afu_in_p=0; buf="$bufc"
-        }
-        BUFFER="$buf"
-        buffer_cur="$bufc"
-    }
-}
-zle -N afu+delete-unambiguous-prefix
-function afu-ad-delete-unambiguous-prefix () {
-    local afufun="$1"
-    local code; code=$functions[$afufun]
-    eval "function $afufun () { zle afu+delete-unambiguous-prefix; $code }"
-}
-afu-ad-delete-unambiguous-prefix afu+accept-line
-afu-ad-delete-unambiguous-prefix afu+accept-line-and-down-history
-afu-ad-delete-unambiguous-prefix afu+accept-and-hold
 
 
 # Enhanced change directory
