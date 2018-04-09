393c6af57aa81cd36566bdba2c984e93b54f089e
u1and0 <e01.ando60@gmail.com>
2018-04-09 12:52:13 +0900

    [mod]@install.sh: change shell & remove HOME .git dir

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 # Install for [dotfiles](https://github.com/u1and0/dotfiles.git)
 
-git clone --recursive https://github.com/u1and0/dotfiles.git
+#git clone --recursive https://github.com/u1and0/dotfiles.git
 for file in `ls -A ${HOME}/dotfiles`; do
     ln -fs ${HOME}/dotfiles/$file ${HOME}
 done
 
-rmdir ${HOME}/.config/ranger
+rmdir ${HOME}/.config/ranger 2> /dev/null
 
 for dir in `ls -A ${HOME}/dotfiles/.config`; do
     ln -fs ${HOME}/dotfiles/.config/$dir ${HOME}/.config
 done
+
+sudo chsh -s `which zsh` `whoami`
+rm -rf ${HOME}/.git
