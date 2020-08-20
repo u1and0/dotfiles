11d6a1a58666fd4ad055701b09d00fcef53f0bce
u1and0 <e01.ando60@gmail.com>
2020-08-20 18:03:41 +0900

    [add] doc/docx preview and pager script @config/ranger

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -58,6 +58,14 @@ case "$extension" in
     pdf)
         try pdftotext -l 10 -nopgbrk -q "$path" - && \
             { dump | trim | fmt -s -w $width; exit 0; } || exit 1;;
+    # DOC documents:
+    doc)
+        try catdoc "$path" && \
+            { dump | trim | fmt -s -w $width; exit 0; } || exit 1;;
+    # DOCX documents:
+    docx)
+        try docx2txt "$path" - && \
+            { dump | trim | fmt -s -w $width; exit 0; } || exit 1;;
     # BitTorrent Files
     torrent)
         try transmission-show "$path" && { dump | trim; exit 5; } || exit 1;;
