3ac13db7dbdbafa9f8e2cbfc4d02632d7c0c3e44
u1and0 <e01.ando60@gmail.com>
2023-06-18 08:05:53 +0900

    [fix] import seaborn, [comment] convert md,py,html

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -45,6 +45,10 @@
 #  Use '*' to allow any origin to access your server.
 #
 #  Takes precedence over allow_origin_pat.
+
+import os
+from subprocess import check_call
+
 c.NotebookApp.allow_origin = '*'
 
 # Use a regular expression for the Access-Control-Allow-Origin header
@@ -513,20 +517,18 @@ c.NotebookApp.open_browser = False
 #  - path: the filesystem path to the file just written - model: the model
 #  representing the file - contents_manager: this ContentsManager instance
 #c.FileContentsManager.post_save_hook = None
-import os
-from subprocess import check_call
 
 
 def post_save(model, os_path, contents_manager):
     """post-save hook for converting notebooks to .py scripts
-    ipynbが保存されるたびに[py, html, md, tex, pdf]を作成する。"""
+    ipynbが保存されるたびに[py, md, html]を作成する。"""
     if model['type'] != 'notebook':
         return  # only do this for notebooks
     d, fname = os.path.split(os_path)
     base, ext = os.path.splitext(fname)
     check_call(['jupyter', 'nbconvert', '--to', 'script', fname], cwd=d)
-    check_call(['jupyter', 'nbconvert', '--to', 'html', fname], cwd=d)
     check_call(['jupyter', 'nbconvert', '--to', 'markdown', fname], cwd=d)
+    check_call(['jupyter', 'nbconvert', '--to', 'html', fname], cwd=d)
     # check_call(['jupyter-nbconvert', '--to', 'latex', fname, '--template', 'jsarticle.tplx'], cwd=d)
     # check_call(['extractbb', base+'_files/*.png'], cwd=d)
     # check_call(['platex', '-interaction=nonstopmode', '-synctex=1', '-kanji=utf8', '-guess-input-enc' , base+'.tex'], cwd=d)
